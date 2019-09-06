6d670b25bc100fcef066a671e2f56d699c34a02d
mmargin <mmargin@umich.edu>
2019-09-06 16:22:47 -0400

    completed lab0

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+import random
+
+print("Maria")
+num1 = random.randint(1,101)
+num2 = random.randint(1,101)
+print(num1)
+print(num2)
+sum = num1 + num2
+print("Sum = " + str(sum))
+print("Average = " + str(sum/2))
